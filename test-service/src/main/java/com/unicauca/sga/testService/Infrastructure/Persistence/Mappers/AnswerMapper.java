bdfd758583ddc98c74f24f768efc03903b86ee22
Daniel Solarte <120216583+DanielSolarte3@users.noreply.github.com>
2025-12-11 18:58:30 -0500

    fix(test microservice): abstract classes mappers compiling

--- test-service/src/main/java/com/unicauca/sga/testService/Infrastructure/Persistence/Mappers/AnswerMapper.java
+++ test-service/src/main/java/com/unicauca/sga/testService/Infrastructure/Persistence/Mappers/AnswerMapper.java
@@ -1,20 +1,43 @@
 package com.unicauca.sga.testService.Infrastructure.Persistence.Mappers;
 
 import com.unicauca.sga.testService.Domain.Models.Answer;
+import com.unicauca.sga.testService.Domain.Models.AnswerTypes.ChoiceAnswer;
+import com.unicauca.sga.testService.Domain.Models.AnswerTypes.OpenEndedAnswer;
 import com.unicauca.sga.testService.Infrastructure.Context.CycleAvoidingMappingContext;
-import com.unicauca.sga.testService.Infrastructure.Persistence.Mappers.AnswerTypesMappers.ChoiceAnswerMapper;
-import com.unicauca.sga.testService.Infrastructure.Persistence.Mappers.AnswerTypesMappers.OpenEndedAnswerMapper;
 import com.unicauca.sga.testService.Infrastructure.Persistence.Tables.AnswerTable;
-import org.mapstruct.Context;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import com.unicauca.sga.testService.Infrastructure.Persistence.Tables.AnswerTypes.ChoiceAnswerTable;
+import com.unicauca.sga.testService.Infrastructure.Persistence.Tables.AnswerTypes.OpenEndedAnswerTable;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring", uses = {
-        ChoiceAnswerMapper.class,
-        OpenEndedAnswerMapper.class,
         QuestionMapper.class
 })
 public interface AnswerMapper {
-    Answer toModel(AnswerTable answerTable, @Context CycleAvoidingMappingContext context);
-    AnswerTable toInfra(Answer answer, @Context CycleAvoidingMappingContext context);
+    ChoiceAnswer ChoiceAnswerToModel(ChoiceAnswerTable answer, @Context CycleAvoidingMappingContext context);
+    ChoiceAnswerTable ChoiceAnswerToInfra(ChoiceAnswer answer, @Context CycleAvoidingMappingContext context);
+
+    OpenEndedAnswer OpenEndedAnswerToModel(OpenEndedAnswerTable answer, @Context CycleAvoidingMappingContext context);
+    OpenEndedAnswerTable OpenEndedAnswerToInfra(OpenEndedAnswer answer, @Context CycleAvoidingMappingContext context);
+
+    default Answer toModel(AnswerTable answer, @Context CycleAvoidingMappingContext context) {
+        if(answer instanceof ChoiceAnswerTable){
+            return ChoiceAnswerToModel((ChoiceAnswerTable) answer, context);
+        }
+        else if(answer instanceof OpenEndedAnswerTable){
+            return OpenEndedAnswerToModel((OpenEndedAnswerTable) answer, context);
+        }
+        else{
+            return null;
+        }
+    }
+
+    default AnswerTable toInfra(Answer answer, @Context CycleAvoidingMappingContext context) {
+        if(answer instanceof ChoiceAnswer){
+            return ChoiceAnswerToInfra((ChoiceAnswer) answer, context);
+        }else if(answer instanceof OpenEndedAnswer){
+            return OpenEndedAnswerToInfra((OpenEndedAnswer) answer, context);
+        }else{
+            return null;
+        }
+    }
 }
